5d674270e43f1659afd3c637a208c5d3f5269324
Lucas Bertuol <151688160+bertua@users.noreply.github.com>
2024-09-04 21:31:18 -0300

    Add files via upload

---
+++ Bodegao/src/main/java/br/com/cdlcode/bodegao/Bodegao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.cdlcode.bodegao;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Bodegao {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
